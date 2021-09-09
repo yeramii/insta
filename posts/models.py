134838144de9a16715d2171db4e46f612d6cd260
yeramii <ramit.noh@gmail.com>
2021-09-09 09:46:21 +0900

    model 정의

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
+class Post(models.Model):
+    image = ProcessedImageField(upload_to='image/',
+                                processors=[ResizeToFill(500, 500)],
+                                format='JPEG',
+                                options={'quality': 100})
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
