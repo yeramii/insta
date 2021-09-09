c5da0cc3d53b580f0258383284bce0470caa3ac4
yeramii <ramit.noh@gmail.com>
2021-09-09 10:28:19 +0900

    Fix create

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/index.html', context)
 
 
 def create(request):
