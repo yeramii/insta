ffc628c8631e0e1138b611ddbc1e095478b518b1
yeramii <ramit.noh@gmail.com>
2021-09-09 10:10:32 +0900

    GET 방식의 create 요청에 대한 기능 완성

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'posts/form.html', context)
